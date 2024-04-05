f7fc6fac3b613650f22a5bc839f631b67e61e391
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-04-05 12:23:43 -0300

    Initial commit

---
+++ src/test/java/com/adega/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adega.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
