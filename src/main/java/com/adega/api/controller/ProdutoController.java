2771cf4b230c8014d4f3c385c9548895a417422b
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-04-19 16:31:06 -0300

    produto controller

---
+++ src/main/java/com/adega/api/controller/ProdutoController.java
@@ -0,0 +1,48 @@
+package com.adega.api.controller;
+
+import com.adega.api.domain.produto.dto.DadosAtualizarProduto;
+import com.adega.api.domain.produto.dto.DadosCadastroProduto;
+import com.adega.api.domain.produto.dto.DadosListProduto;
+import com.adega.api.service.produto.ProdutoService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/produto")
+@RestController
+public class ProdutoController {
+    @Autowired
+    ProdutoService produtoService;
+
+    @PostMapping("/cadastra")
+    @Transactional
+    public ResponseEntity<DadosCadastroProduto> cadastro(@RequestBody DadosCadastroProduto dadosCadastroProduto){
+        return produtoService.cadastroProduto(dadosCadastroProduto);
+
+    }
+
+    @GetMapping("/listar")
+    public Page<DadosListProduto> listProdutos(@PageableDefault(size = 10,sort = {"preco"}) Pageable pageable, @RequestBody DadosListProduto dados){
+
+        return produtoService.listProduto(pageable,dados);
+
+
+    }
+
+
+    @PutMapping("/atualizar/{id}")
+    @Transactional
+    public ResponseEntity<DadosAtualizarProduto> atualizar(@PathVariable Long id ,@RequestBody DadosAtualizarProduto atualizarProduto){
+        return produtoService.atualizarProduto(id ,atualizarProduto);
+
+    }
+
+
+
+
+}
---
+++ src/main/java/com/adega/api/service/produto/ProdutoService.java
@@ -0,0 +1,69 @@
+package com.adega.api.service.produto;
+
+import com.adega.api.domain.categoria.Categoria;
+import com.adega.api.domain.produto.Produto;
+import com.adega.api.domain.produto.dto.DadosAtualizarProduto;
+import com.adega.api.domain.produto.dto.DadosCadastroProduto;
+import com.adega.api.domain.produto.dto.DadosListProduto;
+import com.adega.api.repository.CategoriaRepository;
+import com.adega.api.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    ProdutoRepository produtoRepository;
+    @Autowired
+    CategoriaRepository categoriaRepository;
+
+
+    public ResponseEntity<DadosCadastroProduto> cadastroProduto(DadosCadastroProduto dadosCadastroProduto) {
+       Optional<Categoria> categoria = categoriaRepository.findById(dadosCadastroProduto.idCategoria());
+       if(categoria.isPresent()){
+           produtoRepository.save(new Produto(dadosCadastroProduto,categoria.get()));
+           return ResponseEntity.ok().body(dadosCadastroProduto);
+       }else{
+           throw new RuntimeException("CATEGORIA NÃO VALIDA");
+       }
+    }
+
+    public Page<DadosListProduto> listProduto(Pageable pageable, DadosListProduto dados) {
+        if(dados.id()!=null){
+            return produtoRepository.findById(dados.id(),pageable).map(DadosListProduto::new);
+        }else if(dados.nome()!=null){
+            return produtoRepository.findByNomeContaining(dados.nome(),pageable).map(DadosListProduto::new);
+        }else if(dados.preco()!=null){
+            return produtoRepository.findByPrecoGreaterThanEqual(dados.preco(),pageable).map(DadosListProduto::new);
+        }else if(dados.quantidade()!=null){
+            return produtoRepository.findByQuantidadeGreaterThanEqual(dados.quantidade(),pageable).map(DadosListProduto::new);
+        }else{
+            return produtoRepository.findAll(pageable).map(DadosListProduto::new);
+
+        }
+
+    }
+
+    public ResponseEntity<DadosAtualizarProduto> atualizarProduto(Long id, DadosAtualizarProduto atualizarProduto) {
+        Optional<Categoria> categoria = categoriaRepository.findById(atualizarProduto.idCategoria());
+        Optional<Produto>  produto = produtoRepository.findById(id);
+        if(produto.isPresent()){
+            if (categoria.isEmpty()) {
+                produto.ifPresent(p->p.atualizar(atualizarProduto,null));
+
+            } else {
+                produto.ifPresent(p->p.atualizar(atualizarProduto,categoria.get()));
+
+            }
+            return ResponseEntity.ok().body(atualizarProduto);
+        }else{
+            throw new RuntimeException("NÃO ENCONTRADO PRODUTO OU CATEGORIA");
+        }
+    }
+}
